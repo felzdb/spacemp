e580f7cf7d091e4f0dcc3406e4711aae3935b6ae
lmfrozza <frozzalucca@gmail.com>
2024-10-05 14:55:42 -0300

    .
    hgc

--- blueprints/GPTBot/GPTBot.py
+++ blueprints/GPTBot/GPTBot.py
@@ -2,9 +2,9 @@ import requests
 import json
 
 headers = {
-    "Authorization": f"Bearer {""}"
+    "Authorization": "Bearer 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"
 }
-link = "a"
+link = "https://api.openai.com/v1/models"
 requisicao = requests.get(link, headers=headers)
 
 print(requisicao)
