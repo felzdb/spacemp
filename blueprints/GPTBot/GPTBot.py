b135f7a8ccf3db7eb0e916f01f0685af6ba9ce81
lmfrozza <frozzalucca@gmail.com>
2024-10-05 14:42:54 -0300

    .

--- blueprints/GPTBot/GPTBot.py
+++ blueprints/GPTBot/GPTBot.py
@@ -2,10 +2,12 @@ import requests
 import json
 
 headers = {
-    "Authorization": f"Bearer {"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"}"
+    "Authorization": f"Bearer {""}"
 }
 
 requisicao = requests.get("https://api.openai.com/v1/models", headers=headers)
 
 print(requisicao)
 print(requisicao.text)
+
+id_modelo = "gpt-3.5-turbo"
