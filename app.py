2dc646a45a125670e07782b27ba17219203dda74
Dierlen Hallze Alexandre <dierlen84@gmail.com>
2024-10-05 20:10:48 -0300

    Alinhando codigos

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ import pandas as pd
 
 from blueprints.Login.login import login_bp
 from blueprints.Register.register import register_bp
+from blueprints.Dash_temp.dash_temp import dash_temp_bp
+
 
 app = Flask(__name__)
 app.secret_key = 'sua_chave_secretaa'
 
 app.register_blueprint(login_bp)
 app.register_blueprint(register_bp)
+app.register_blueprint(dash_temp_bp)
 
 # Função para verificar credenciais
 def verificar_usuario(username, password):
