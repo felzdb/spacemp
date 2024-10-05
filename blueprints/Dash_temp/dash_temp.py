a929e8dfa9ba5f1e64b14b9b506b868ddda014d4
Dierlen Hallze Alexandre <dierlen84@gmail.com>
2024-10-05 20:04:59 -0300

    Inicio de dash board

--- blueprints/Dash_temp/dash_temp.py
+++ blueprints/Dash_temp/dash_temp.py
@@ -1,16 +1,12 @@
 import requests
-
+import datetime
 from flask import Blueprint, render_template, redirect
 
 dash_temp_bp = Blueprint("dash_temp_menu", __name__, template_folder="templates")
 
-@dash_temp_bp.route("/dashboard")
-def route_HomePage():
-    return render_template('dash_temp.html')
-
 
 api_weather= '94d84e5dff66fb4d5c74a3b6bb0f8f25'
-city = 'flores da cunha'
+city = 'caxias do sul'
 
 url_coord = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={api_weather}"
 
@@ -47,6 +43,26 @@ if response.status_code == 200:
     temp_min = data['main']['temp_min']
     temp_max = data['main']['temp_max']
     clima = data['weather'][0]['description']
+    clima = clima.title()
+    date_time = datetime.datetime.utcfromtimestamp(data['dt'])
+    day_of_week = date_time.strftime('%A')
+    icon_code = data['weather'][0]['main'] 
+
+    # Exemplo de mapeamento de 'main' para ícones de Font Awesome
+    weather_icon_mapping = {
+        'Clear': 'fa-sun',
+        'Clouds': 'fa-cloud',
+        'Rain': 'fa-cloud-showers-heavy',
+        'Drizzle': 'fa-cloud-rain',
+        'Thunderstorm': 'fa-bolt',
+        'Snow': 'fa-snowflake',
+        'Mist': 'fa-smog',
+        'Fog': 'fa-smog',
+        'Haze': 'fa-smog'
+    }
+
+    # Buscar o ícone correspondente
+    icon_class = weather_icon_mapping.get(icon_code, 'fa-question')
 
    # Exibindo os valores
     print(f"Clima: {clima}")
@@ -59,3 +75,7 @@ if response.status_code == 200:
 else:
     # Imprimindo o código de status caso haja erro
     print(f"Erro na requisição: {response.status_code}")
+
+@dash_temp_bp.route("/dashboard")
+def route_HomePage():
+    return render_template('dash_temp.html',city=city,temp=temp,clima=clima,temp_min=temp_min,temp_max=temp_max,day=day_of_week,icon=icon_class)
--- blueprints/Dash_temp/testes de api.py
+++ blueprints/Dash_temp/testes de api.py
@@ -1,19 +1,23 @@
 import requests
+import datetime
 
 api_weather= '94d84e5dff66fb4d5c74a3b6bb0f8f25'
 lat = -29.03
 lon = -51.18
 
-url = f"https://api.openweathermap.org/data/2.5/forecast/climate?lat={lat}&lon={lon}&cnt=7&appid={api_weather}"
+url_current = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={api_weather}&lang=pt_br"
 
 # Fazendo a requisição para a API
-response = requests.get(url)
+response = requests.get(url_current)
 
 # Verifica se a requisição foi bem sucedida
 if response.status_code == 200:
     data = response.json()
+    date_time = datetime.datetime.utcfromtimestamp(data['dt'])
+    day_of_week = date_time.strftime('%A')
 
     print(data)
+    print(day_of_week)
 else:
     # Imprimindo o código de status caso haja erro
     print(f"Erro na requisição: {response.status_code}")
