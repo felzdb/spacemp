a0b4326db0bf7a4951ca4a32a18df6f8f5e27c6c
lmfrozza <frozzalucca@gmail.com>
2024-10-05 13:52:55 -0300

    .

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, blueprints
 
 
 from blueprints.Login.login import login_bp
+from blueprints.Register.register import register_bp
 app = Flask(__name__)
 
 app.register_blueprint(login_bp)
+app.register_blueprint(register_bp)
 
 
 #//URL INVÁLIDA//#
