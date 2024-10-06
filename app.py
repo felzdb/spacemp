ca15a47080ce1c1bd8b047c3283c7e0c95049abc
Felz <bisottofelipe643@gmail.com>
2024-10-05 21:16:10 -0300

    inicio weather

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from blueprints.Login.login import login_bp
 from blueprints.HomePage.HomePage import HomePage_bp
 from blueprints.Register.register import register_bp
 from blueprints.Dash_temp.dash_temp import dash_temp_bp
+from blueprints.SpaceWeather.space_weather import space_weather_bp
+
 
 
 app = Flask(__name__)
@@ -16,6 +18,7 @@ app.register_blueprint(login_bp)
 app.register_blueprint(register_bp)
 app.register_blueprint(dash_temp_bp)
 app.register_blueprint(HomePage_bp)
+app.register_blueprint(space_weather_bp)
 
 @app.route("/")
 def route_HomePage():
---
+++ blueprints/SpaceWeather/space_weather.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, jsonify, render_template
+import requests
+
+space_weather_bp = Blueprint('space_weather', __name__)
+
+# Defina sua chave de API aqui
+API_KEY = 'krtHTpfgfbNy56GfF1n8CBgCdL5Okfg9Pv726TLT'  # Substitua pela sua chave de API
+
+@space_weather_bp.route('/space_weather')
+def space_weather():
+    url = f'https://api.nasa.gov/planetary/apod?api_key={API_KEY}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return render_template('weather.html', data=data)
+    else:
+        return jsonify({"error": "Unable to fetch data"}), response.status_code
+
+@space_weather_bp.route('/space_weather/data')
+def space_weather_data():
+    url = f'https://api.nasa.gov/planetary/apod?api_key={API_KEY}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify(data)
+    else:
+        return jsonify({"error": "Unable to fetch data"}), response.status_code
