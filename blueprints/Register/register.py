e2e09ce048fc61c35d693e72247f58816c6c2029
Dierlen Bandeira <dierlen84@gmail.com>
2024-10-05 22:29:05 -0300

    registor works

--- blueprints/Register/register.py
+++ blueprints/Register/register.py
@@ -1,12 +1,57 @@
-from flask import Blueprint, render_template, redirect, request, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint
+import requests
+import json
 
+register_bp = Blueprint("Register_Menu", __name__, template_folder="templates")
 
-register_bp = Blueprint("register", __name__, template_folder="templates")
+@register_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Capturando os dados do formulário
+        nome_completo = request.form['nome_completo']
+        nome_usuario = request.form['nome_usuario']
+        email = request.form['user_email']
+        telefone = request.form['telefone']
+        senha = request.form['senha']
 
-@register_bp.route("/register", methods=["POST", "GET"])
-def route_register():
-    if(request.method == "POST"):
-        redirect("/login")
-    else:
-        return render_template('register.html')
-    
+        # Exibindo os dados recebidos no terminal para fins de debug
+        print(f"Registrando novo usuário: {nome_completo}, {nome_usuario}, {email}, {telefone}")
+
+        # Dados que serão enviados para o Airtable
+        data = {
+            "records": [
+                {
+                    "fields": {
+                        "Name": nome_completo,
+                        "User_name": nome_usuario,
+                        "Email": email,
+                        "Phone": telefone,
+                        "Password": senha  # Armazenar de forma segura usando hashing em uma aplicação real
+                    }
+                }
+            ]
+        }
+
+        # Configurações do Airtable
+        pat = 'patqTjSfqmO03jEDd.4330e2c445694f5b08b9223c4c869eb4b19c1c903dd58ff8b409aefd3dabe73a'
+        base_id = 'appAjKZG2ceyIn0FU'
+        table_name = 'tbluUrT38tFYLVEBQ'
+        airtable_url = f'https://api.airtable.com/v0/{base_id}/{table_name}'
+        headers = {
+            'Authorization': f'Bearer {pat}',
+            'Content-Type': 'application/json'
+        }
+
+        # Enviando os dados para o Airtable
+        response = requests.post(airtable_url, headers=headers, data=json.dumps(data))
+
+        # Verificando a resposta da API do Airtable
+        if response.status_code == 200:
+            flash('Usuário registrado com sucesso!', 'success')
+            return redirect(url_for('Login_Menu.route_login'))
+        else:
+            flash('Erro ao registrar usuário. Tente novamente.', 'error')
+            return redirect(url_for('Register_Menu.route_register'))
+
+    # Renderizando o template do formulário de registro quando a página for acessada via GET
+    return render_template('register.html')
